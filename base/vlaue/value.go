6f5f970929afd0c61a99503297065761b743e443
wangning <2631015638@qq.com>
2022-01-15 23:06:53 +0800

    practice value

---
+++ base/vlaue/value.go
@@ -0,0 +1,17 @@
+// 值
+package main
+
+import "fmt"
+
+func main() {
+	// 字符串链接
+	fmt.Println("go" + "lang")
+
+	//整数和浮点型
+	fmt.Println("1+1=", 1+1)
+
+	//布尔类型
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+
+}
